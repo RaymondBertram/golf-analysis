6d2263a3310288a46c2958543e0c96021c88f67b
Raymond Bertram <raymond.bertram@gmx.de>
2024-11-17 18:15:47 +0100

    feature/authentication-supabase - wip

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,21 @@
-import { login, signup } from './actions'
+import { login, signup } from './actions';
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
-  )
+    <div className="ge-container flex h-full flex-col items-center justify-center">
+      <h1 className="mb-6">Login</h1>
+      <form className="flex w-full max-w-md flex-col">
+        <label htmlFor="email">Email:</label>
+        <input className="input mb-6" id="email" name="email" type="email" required />
+        <label htmlFor="password">Password:</label>
+        <input className="input mb-6" id="password" name="password" type="password" required />
+        <button className="btn-primary mb-3" formAction={login}>
+          Log in
+        </button>
+        <button className="btn-secondary" formAction={signup}>
+          Sign up
+        </button>
+      </form>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,7 @@
 'use client';
 import { TableData } from '@/model';
-import { Bar } from 'react-chartjs-2';
 
 export default function Home() {
-  const data = [
-    {
-      id: 1,
-      year: 2016,
-      userGain: 80000,
-      userLost: 823,
-    },
-    {
-      id: 2,
-      year: 2017,
-      userGain: 45677,
-      userLost: 345,
-    },
-    {
-      id: 3,
-      year: 2018,
-      userGain: 78888,
-      userLost: 555,
-    },
-    {
-      id: 4,
-      year: 2019,
-      userGain: 90000,
-      userLost: 4555,
-    },
-    {
-      id: 5,
-      year: 2020,
-      userGain: 4300,
-      userLost: 234,
-    },
-  ];
-
   const datastructure: TableData[] = [
     {
       club: 'GC Buchholz',
@@ -217,7 +183,6 @@ export default function Home() {
       </section>
       <section id="soloperformance">
         <h2>Soloperformance</h2>
-        <Bar data={data} />
       </section>
     </div>
   );
